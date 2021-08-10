0fb167526405bade21888693c4b7345b3fefcf56
fifax23 <galaxymexx@gmail.com>
2021-08-11 01:08:47 +0600

    v1

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,79 @@
 class TicTacToe {
-    constructor() {
+  field = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+  currentPlayerSymbol = 'x';
+  symbols = ['x', 'o'];
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayerSymbol;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.field[rowIndex][columnIndex] === null) {
+      this.field[rowIndex][columnIndex] = this.currentPlayerSymbol;
+      this.currentPlayerSymbol = this.symbols.reverse()[0];
+    }
+  }
+
+  isFinished() {
+    const isWinnner = this.getWinner();
+    const isDraw = this.isDraw();
+    return isWinnner || isDraw ? true : false;
+  }
+
+  getWinner() {
+    // Horizontal and Vertical
+    let winnerSymbol;
+    let isWinner = false;
+    for (let i = 0; i < this.field.length; i++) {
+      winnerSymbol = this.field[i][0];
+      isWinner = this.field[i].every((el) => el === winnerSymbol);
+      if (isWinner) {
+        return winnerSymbol;
+      }
+      winnerSymbol = this.field[0][i];
+      isWinner = this.field.every((array) => array[i] === winnerSymbol);
+      if (isWinner) {
+        return winnerSymbol;
+      }
     }
 
-    getCurrentPlayerSymbol() {
-
+    //Diagonal
+    switch (true) {
+      case this.field[0][0] == this.field[1][1] &&
+        this.field[0][0] == this.field[2][2]:
+        return this.field[0][0];
+      case this.field[0][2] == this.field[1][1] &&
+        this.field[0][2] == this.field[2][0]:
+        return this.field[0][2];
     }
 
-    nextTurn(rowIndex, columnIndex) {
+    return null;
+  }
 
+  noMoreTurns() {
+    for (let i = 0; i < this.field.length; i++) {
+      for (let j = 0; j < this.field[i].length; j++) {
+        if (this.field[i][j] === null) {
+          return false;
+        }
+      }
     }
+    return true;
+  }
 
-    isFinished() {
+  isDraw() {
+    const isWinnner = this.getWinner();
+    const isNoMoreTurns = this.noMoreTurns();
+    return isNoMoreTurns && !isWinnner ? true : false;
+  }
 
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
